6e4dbe0d260bff2c7f69ee6831b4d0745617ba41
Your name here <isaacxpreston7@gmail.com>
2021-11-03 14:30:55 -0400

    fix repl

--- repl.sh
+++ repl.sh
@@ -2,9 +2,22 @@
 
 repl () { 
   temp=$(mktemp --suffix .sh)
-  printf "#!/usr/local/bin/bash\n" > $temp
-  micro --autosave 1 "$temp:2"
-  chmod +x $temp
-  . $temp
+  printf "#!/usr/local/bin/bash\n\n\n" > $temp
+
+  micro --autosave 1 --parsecursor true "$temp:3"
+  chmod +x $temp && . $temp
+
+  prompt () {
+    read -p "filename: " filename
+    [[ ! -z "$filename" ]] || prompt
+  }
+
+  read -p "enter 'y' to save: " save
+  [[ "$save" == "y" ]] &&
+    prompt &&
+    cp $temp "$(pwd)/$filename"
+
   rm $temp
 }
+
+repl
