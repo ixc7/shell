13d742d2197d410e4f0e6db16c80955dd8df3d1e
Your name here <isaacxpreston7@gmail.com>
2021-11-05 07:07:41 -0400

    readmes.sh cleanup

--- readmes.sh
+++ readmes.sh
@@ -1,20 +1,24 @@
 #!/usr/local/bin/bash
 
-cmd="bat"
-depth=3
+limit=3
+width=95
+cmd="
+  bat
+    --style=header,grid,numbers
+    --terminal-width $(($(($(tput cols) / 100)) * $width))
+"
 
 getFiles () {
-  list=$(tree -f -i -I "node_modules|.git*|.DS_Store" -a -L $depth)
+  list=$(tree -f -i -I "node_modules|.git*|.DS_Store" -a -L $limit)
   count=$(echo "$list" | rga -e README --count)
   paths=$(echo "$list" | rga -e README)
   [[ "$count" -gt 0 ]] && echo "$paths"
 }
 
 cmdFiles () {
-  [[ ! -z "$@" ]] && cmd="$@"
   files=$(getFiles | tr '\n' ' ')
-  [[ ! -z "$files" ]] &&
-  $cmd $files
+  [[ ! -z "$@" ]] && cmd="$@"
+  [[ ! -z "$files" ]] && $cmd $files
 }
 
-cmdFiles "$@"
+cmdFiles "$@" && clear
