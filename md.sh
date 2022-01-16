2f0e96b608bf8891f084a438e607a7d71ed63d2b
Your name here <isaacxpreston7@gmail.com>
2022-01-16 17:29:21 -0500

    open readme files by default md.sh

--- md.sh
+++ md.sh
@@ -1,9 +1,14 @@
 #!/usr/local/bin/bash
 
-[[ -z "${@}" ]] && exit 0
-
 args=false
 files=""
+
+if [[ -z ${@} ]]; then
+  if [[ $(ls | rga -e readme | wc -l) != 0 ]]; then
+    cat $(ls | rga -e readme | tr '\n' ' ') | highlight -s base16/macintosh -S pdf -O xterm256 | less -r
+  fi
+  exit 0
+fi
 
 for arg in ${@}; do
   if [[ ! -f ${arg} ]] && [[ ! -d ${arg} ]]; then
