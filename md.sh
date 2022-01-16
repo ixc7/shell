f32ce8283e2b4fb217164a577da80428da8eda3b
Your name here <isaacxpreston7@gmail.com>
2022-01-16 16:56:02 -0500

    md.sh

---
+++ md.sh
@@ -0,0 +1,31 @@
+#!/usr/local/bin/bash
+
+[[ -z "${@}" ]] && exit 0
+
+args=false
+files=""
+
+for arg in ${@}
+do
+  if [[ ! -f ${arg} ]] && [[ ! -d ${arg} ]]
+  then
+    args=true
+  fi
+
+  if [[ -f ${arg} ]]
+  then
+    files="${files} ${arg}"
+  fi
+done
+
+if [[ ${args} == false ]]
+then
+  if [[ ${#files} != 0 ]]
+  then
+    cat ${files} | highlight -s base16/macintosh -S pdf -O xterm256 | less -r
+  else
+    exit 0
+  fi
+else
+  ${@} | highlight -s base16/macintosh -S pdf -O xterm256 | less -r
+fi
