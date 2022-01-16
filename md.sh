7c101fef9947bb6aaf2067efce0b2df7575bbaa0
Your name here <isaacxpreston7@gmail.com>
2022-01-16 16:57:25 -0500

    md.sh

--- md.sh
+++ md.sh
@@ -5,26 +5,19 @@
 args=false
 files=""
 
-for arg in ${@}
-do
-  if [[ ! -f ${arg} ]] && [[ ! -d ${arg} ]]
-  then
+for arg in ${@}; do
+  if [[ ! -f ${arg} ]] && [[ ! -d ${arg} ]]; then
     args=true
   fi
 
-  if [[ -f ${arg} ]]
-  then
+  if [[ -f ${arg} ]]; then
     files="${files} ${arg}"
   fi
 done
 
-if [[ ${args} == false ]]
-then
-  if [[ ${#files} != 0 ]]
-  then
+if [[ ${args} == false ]]; then
+  if [[ ${#files} != 0 ]]; then
     cat ${files} | highlight -s base16/macintosh -S pdf -O xterm256 | less -r
-  else
-    exit 0
   fi
 else
   ${@} | highlight -s base16/macintosh -S pdf -O xterm256 | less -r
