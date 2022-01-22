37f50f4a7dc9a79036915c60ba43efc3995407aa
Your name here <isaacxpreston7@gmail.com>
2022-01-22 12:02:40 -0500

    md.sh

--- md.sh
+++ md.sh
@@ -1,39 +1,70 @@
 #!/usr/local/bin/bash
 
+trap cleanup EXIT
+
 default="readme"
 nonPaths=false
 list=""
+lang="pdf"
+temp=$(mktemp)
 
-parser () {
-  ${@} | highlight -s base16/macintosh -S pdf -O xterm256 | less -r
+cleanup () {
+  # rm -f ${temp} && echo -e "\x1Bc\x1b[?25h"
+  # rm -f ${temp} && echo -ne "\x1b[?25h"
+  rm -f ${temp} && la
+  exit 0
 }
 
-# if no args, cat readmes* | highlight
+pager () {
+  ${@} | highlight -s base16/macintosh -S ${lang} -O xterm256 | less -r
+}
+
+format () {
+  echo -e "
+    \r$(figlet -f miniwi ${1})
+    \n\r$(for i in $(seq $(tput cols)); do  echo -ne -; done)
+  " > ${temp} &&
+  cat ${1} >> ${temp} &&
+  echo >> ${temp}
+}
+
+setLang () {
+  lang=$(basename "${1}" | tr "." "\n" | tail -n 1)
+}
+
+render () {
+  echo -ne "\x1Bc\x1b[3J\x1b[?25l" &&
+  setLang ${1} &&
+  format ${1} &&
+  pager cat ${temp} 
+}
+
+# if no args, format and display ${default}* and exit
 if [[ -z ${@} ]]; then
-  if [[ $(ls | rga -e "${default}" | wc -l) -gt 0 ]]; then
-    parser cat $(ls | rga -e "${default}" | tr '\n' ' ')
-  fi
+    for file in $(ls | rga -e "${default}"); do
+      [[ -f ${file} ]] && render ${file}
+    done
   exit 0
 fi
 
-# else, determine if args are all paths
+# else, determine kind of args
 for arg in ${@}; do
-  # if not a path to something, stop checking
+  # if not a path, stop checking
   if [[ ! -f ${arg} ]] && [[ ! -d ${arg} ]]; then
     nonPaths=true
     break
   fi
-  # if file or dir, add to list
+  # else, add to list
   [[ -f ${arg} ]] && list="${list} ${arg}"
   [[ -d ${arg} ]] && list="${list} ${arg}/*"
 done
 
-# if mixed args
+# if non paths, pass args directly
 if [[ ${nonPaths} == true ]]; then
-  parser ${@}
-# else, if paths only
-elif [[ ${#list} -gt 0 ]]; then
+  pager ${@}
+# else, format and display files
+else
   for file in ${list}; do
-    parser cat ${file}
+    [[ -f ${file} ]] && render ${file}
   done
 fi
